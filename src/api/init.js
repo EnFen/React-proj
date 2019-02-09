76c0bc1acfbc7d3845bc6b267a36b298e7e70fa2
Nathan <nathan.fenimore@gmail.com>
2019-02-09 14:05:25 +1000

    testing new client

--- src/api/init.js
+++ src/api/init.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    // process.env.NODE_ENV === "production"
-    //   ? "https://wbgs-server.herokuapp.com"
-    //   : "http://localhost:3000",
-    "https://wbgs-server.herokuapp.com",
+    process.env.NODE_ENV === "production"
+      ? "https://wbgs-server.herokuapp.com"
+      : "http://localhost:3000",
+  // "https://wbgs-server.herokuapp.com",
   headers: { "Content-Type": "application/json" },
   proxy: false,
   withCredentials: true
